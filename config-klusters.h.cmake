5f7c39787b6519d0e0be37985f30a02f57e71c03
Montel Laurent <montel@kde.org>
2013-07-16 16:54:04 +0200

    Install autogenerate html file. Nobody updated it from long time ago
    and user from windows/MacOsX doesn't have tool for generate it.
    so better to use generated file.
    If a people wants a day to update it it can install kdelibs3 docbook and adapt it.
    but for me it's better to restart from scratch

--- config-klusters.h.cmake
+++ config-klusters.h.cmake
@@ -1,3 +1,3 @@
 #define KLUSTERS_VERSION "2.0.0"
 
-#define KLUSTER_DOC_PATH "${CMAKE_INSTALL_PREFIX}/share/doc/klusters/"
+#define KLUSTER_DOC_PATH "${CMAKE_INSTALL_PREFIX}/share/doc/klusters/HTML/en/"
